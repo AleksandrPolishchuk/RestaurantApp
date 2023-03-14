f99f8a7a50de087443611b6aeae30eb369d8de4e
AP <aleksandrpolishchuk@gmail.com>
2023-03-14 23:56:47 +0700

    add OrderController

---
+++ backend/RestaurantAPI/RestaurantAPI/Controllers/OrderController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly RestaurantDbContext _context;
+
+        public OrderController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Order
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderMaster>>> GetOrderMasters()
+        {
+          if (_context.OrderMasters == null)
+          {
+              return NotFound();
+          }
+            return await _context.OrderMasters.ToListAsync();
+        }
+
+        // GET: api/Order/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderMaster>> GetOrderMaster(long id)
+        {
+          if (_context.OrderMasters == null)
+          {
+              return NotFound();
+          }
+            var orderMaster = await _context.OrderMasters.FindAsync(id);
+
+            if (orderMaster == null)
+            {
+                return NotFound();
+            }
+
+            return orderMaster;
+        }
+
+        // PUT: api/Order/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderMaster(long id, OrderMaster orderMaster)
+        {
+            if (id != orderMaster.OrderMasterId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderMaster).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderMasterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Order
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OrderMaster>> PostOrderMaster(OrderMaster orderMaster)
+        {
+          if (_context.OrderMasters == null)
+          {
+              return Problem("Entity set 'RestaurantDbContext.OrderMasters'  is null.");
+          }
+            _context.OrderMasters.Add(orderMaster);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrderMaster", new { id = orderMaster.OrderMasterId }, orderMaster);
+        }
+
+        // DELETE: api/Order/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderMaster(long id)
+        {
+            if (_context.OrderMasters == null)
+            {
+                return NotFound();
+            }
+            var orderMaster = await _context.OrderMasters.FindAsync(id);
+            if (orderMaster == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderMasters.Remove(orderMaster);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderMasterExists(long id)
+        {
+            return (_context.OrderMasters?.Any(e => e.OrderMasterId == id)).GetValueOrDefault();
+        }
+    }
+}
